78fa6202a226ecc38e21a9c7c8b55edfb8576e4e
toddlerya <toddlerya@gmail.com>
2017-09-09 15:40:04 +0800

    update itcast_demo

--- itcast/itcast/items.py
+++ itcast/itcast/items.py
@@ -10,5 +10,6 @@ import scrapy
 
 class ItcastItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    title = scrapy.Field()
+    info = scrapy.Field()
